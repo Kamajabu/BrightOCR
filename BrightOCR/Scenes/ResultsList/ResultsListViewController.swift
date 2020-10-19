01193460305e9d96182743037ddf4387ec9c956b
Kamajabu <kamil.buczel@kamajabu.pl>
2020-10-19 16:52:35 +0200

    Added removing highlight on cell after returning to viewController

--- BrightOCR/Scenes/ResultsList/ResultsListViewController.swift
+++ BrightOCR/Scenes/ResultsList/ResultsListViewController.swift
@@ -55,6 +55,10 @@ final class ResultsListViewController: UIViewController {
     required init?(coder _: NSCoder) {
         fatalError("init(coder:) has not been implemented")
     }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        tableView.selectRow(at: nil, animated: false, scrollPosition: .none)
+    }
 
     override func viewDidLoad() {
         super.viewDidLoad()
