b6ca7bd51feeac2972c662a7f100cbd596a0c131
Kamajabu <kamil.buczel@kamajabu.pl>
2020-10-19 16:51:32 +0200

    Added custom rounded button

---
+++ BrightOCR/Shared/RoundedButton.swift
@@ -0,0 +1,44 @@
+//
+//  RoundedButton.swift
+//  BrightOCR
+//
+//  Created by Kamajabu on 19/10/2020.
+//
+
+import UIKit
+
+final class RoundedButton: UIButton {
+    
+    private lazy var shapeLayer: CAShapeLayer = {
+        let shapeLayer = CAShapeLayer()
+        shapeLayer.path = UIBezierPath(roundedRect: bounds, cornerRadius: 20).cgPath
+        shapeLayer.fillColor = customBackgroundColor.cgColor
+        
+        shapeLayer.borderColor = borderColor.cgColor
+        
+        shapeLayer.shadowColor = UIColor.darkGray.cgColor
+        shapeLayer.shadowPath = shapeLayer.path
+        shapeLayer.shadowOffset = CGSize(width: 2.0, height: 2.0)
+        shapeLayer.shadowOpacity = 0.8
+        shapeLayer.shadowRadius = 2
+        
+        return shapeLayer
+    }()
+    
+    private let borderColor = UIColor.adaptedColor(light: UIColor.prussianBlue, dark: .white)
+    private let customBackgroundColor = UIColor.adaptedColor(light: UIColor.redSalsa, dark: .darkSkyBlue)
+    
+    override func layoutSubviews() {
+        super.layoutSubviews()
+        
+        if layer.sublayers?.contains(shapeLayer) == false {
+            layer.insertSublayer(shapeLayer, at: 0)
+        }
+    }
+    
+    func updateColors() {
+        shapeLayer.fillColor = customBackgroundColor.cgColor
+        shapeLayer.borderColor = borderColor.cgColor
+    }
+    
+}
