c6989cd4f71caff2b7348cbc7983eac170b743e1
Kamajabu <kamil.buczel@kamajabu.pl>
2020-10-19 15:54:40 +0200

    Added ResultsListRouter

--- BrightOCR/Scenes/ResultsList/ResultsListCoordinator.swift
+++ BrightOCR/Scenes/ResultsList/ResultsListCoordinator.swift
@@ -7,6 +7,10 @@
 
 import UIKit
 
+protocol ResultsListRouter: class {
+    func showResult(for result: OCRResultModel)
+}
+
 final class ResultsListCoordinator: Coordinator {
     
     private unowned let mainStore: MainStore
@@ -24,7 +28,18 @@ final class ResultsListCoordinator: Coordinator {
         let resultsListViewController = ResultsListViewController(viewModel: resultsListViewModel)
         resultsListViewController.title = "OCR Records"
         
+        resultsListViewController.router = self
+        
         parentViewController?.pushViewController(resultsListViewController, animated: true)
         self.resultsListViewController = resultsListViewController
+    }
+}
+
+extension ResultsListCoordinator: ResultsListRouter {
+    func showResult(for result: OCRResultModel) {
+        let viewModel = ResultDetailsViewModel(result: result, mainStore: mainStore)
+        let viewController = ResultDetailsViewController(viewModel: viewModel)
+        
+        parentViewController?.pushViewController(viewController, animated: true)
     }
 }
