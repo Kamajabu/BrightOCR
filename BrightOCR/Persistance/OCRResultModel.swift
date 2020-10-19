aabdbe68770ae9f2188a91c588b60f7d0b8bd9d4
Kamajabu <kamil.buczel@kamajabu.pl>
2020-10-19 15:20:33 +0200

    Added convenience initialisers to OCRResultModel

--- BrightOCR/Persistance/OCRResultModel.swift
+++ BrightOCR/Persistance/OCRResultModel.swift
@@ -7,8 +7,29 @@
 
 import Foundation
 
+enum MappingError: Error {
+    case missingData
+}
+
 struct OCRResultModel {
     let id: UUID
     let createdOn: Date
     let ocrText: String?
+    
+    init(_ coreDataObject: OCRResult) throws {
+        guard let id = coreDataObject.id,
+              let date = coreDataObject.createdOn else {
+            throw MappingError.missingData
+        }
+        
+        self.id = id
+        self.createdOn = date
+        self.ocrText = coreDataObject.ocrText
+    }
+    
+    init(id: UUID, createdOn: Date, ocrText: String?) {
+        self.id = id
+        self.createdOn = createdOn
+        self.ocrText = ocrText
+    }
 }
