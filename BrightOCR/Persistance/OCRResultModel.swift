93dc086f7350cd3518eec74ed95510ce4563c6d3
Kamajabu <kamil.buczel@kamajabu.pl>
2020-10-19 10:09:23 +0200

    Added OCRResultModel

---
+++ BrightOCR/Persistance/OCRResultModel.swift
@@ -0,0 +1,14 @@
+//
+//  OCRResultModel.swift
+//  BrightOCR
+//
+//  Created by Kamajabu on 19/10/2020.
+//
+
+import Foundation
+
+struct OCRResultModel {
+    let id: UUID
+    let createdOn: Date
+    let ocrResult: String?
+}
