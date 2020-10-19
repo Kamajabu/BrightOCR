4fa6f6108c525b0784589020490a7b76ccdbb0b2
Kamajabu <kamil.buczel@kamajabu.pl>
2020-10-19 15:54:02 +0200

    Added functions for saving and loading records from coreData

--- BrightOCR/Scenes/ResultsList/ResultsListViewModel.swift
+++ BrightOCR/Scenes/ResultsList/ResultsListViewModel.swift
@@ -16,20 +16,50 @@ final class ResultsListViewModel {
     
     init(store: MainStore) {
         self.store = store
+        
+        resultsHistory = store.loadOCRResults()
+    }
+    
+    func saveResult(_ image: UIImage, ocrText: String?) -> OCRResultModel {
+        
+        let key = UUID()
+        do {
+            try store.imageStorage?.setImage(image, for: key)
+        } catch {
+            // Add logger
+        }
+        
+        let model = OCRResultModel(id: key, createdOn: Date(), ocrText: ocrText)
+        store.persistOCRResultModel(model)
+
+        return model
+    }
+    
+    func historyRecord(for indexRow: Int) -> OCRResultModel? {
+        guard resultsHistory.indices.contains(indexRow) else {
+            // Add logger
+            return nil
+        }
+        
+        return resultsHistory[indexRow]
     }
     
     func analyze(image: UIImage, completion: @escaping (Result<OCRResultModel, Error>) -> Void) {
-        ocrEngine.performOCR(on: image) { [weak self] result in
+        ocrEngine.performOCR(on: image) { result in
             switch result {
             case let .failure(error):
                 // Add logger
                 completion(.failure(error))
             case let .success(text):
-                let model = OCRResultModel(id: UUID(), createdOn: Date(), ocrText: text)
-                self?.resultsMockData.append(model)
+                let model = self.saveResult(image, ocrText: text)
+                self.addModelToHistoryList(model)
                 completion(.success(model))
             }
         }
+    }
+    
+    private func addModelToHistoryList(_ model: OCRResultModel) {
+        resultsHistory.append(model)
     }
     
 }
