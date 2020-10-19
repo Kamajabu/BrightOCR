f1e22da3ca469f8dbd1430e621c8fe527a15fa25
Kamajabu <kamil.buczel@kamajabu.pl>
2020-10-19 15:11:45 +0200

    Added coreDataStack as DAO

---
+++ BrightOCR/Store/CoreDataStack.swift
@@ -0,0 +1,27 @@
+//
+//  CoreDataStack.swift
+//  BrightOCR
+//
+//  Created by Kamajabu on 19/10/2020.
+//
+
+import CoreData
+
+final class CoreDataStack {
+    private static let containerName = "OCRImagesData"
+
+    var managedObjectContext: NSManagedObjectContext {
+        return self.persistentContainer.viewContext
+    }
+
+   private  lazy var persistentContainer: NSPersistentContainer = {
+        let container = NSPersistentContainer(name: CoreDataStack.containerName)
+        container.loadPersistentStores(completionHandler: { _, error in
+            if let error = error as NSError? {
+                // Add logger
+            }
+        })
+        return container
+    }()
+
+}
