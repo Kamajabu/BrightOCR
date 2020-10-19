fafc46e62f279d2f4034da90345a7b48188a5248
Kamajabu <kamil.buczel@kamajabu.pl>
2020-10-19 14:59:20 +0200

    Added analyze function using ocrEngine

--- BrightOCR/Scenes/ResultsList/ResultsListViewModel.swift
+++ BrightOCR/Scenes/ResultsList/ResultsListViewModel.swift
@@ -5,12 +5,13 @@
 //  Created by Kamajabu on 19/10/2020.
 //
 
-import Foundation
+import UIKit
 
 final class ResultsListViewModel {
     
     private unowned let store: MainStore
-    
+    private let ocrEngine = FirebaseMLKitOCR()
+
     let mock1 = OCRResultModel(id: UUID(), createdOn: Date(), ocrResult: "Test 1")
     let mock2 = OCRResultModel(id: UUID(), createdOn: Date(timeIntervalSince1970: 8547892146), ocrResult: "Test 2")
     let mock3 = OCRResultModel(id: UUID(), createdOn: Date(timeIntervalSince1970: 43242321), ocrResult: "Test 3")
@@ -19,6 +20,20 @@ final class ResultsListViewModel {
     
     init(store: MainStore) {
         self.store = store
+    }
+    
+    func analyze(image: UIImage, completion: @escaping (Result<OCRResultModel, Error>)->Void) {
+        ocrEngine.performOCR(on: image) { [weak self] result in
+            switch result {
+            case let .failure(error):
+                // Add logger
+                completion(.failure(error))
+            case let .success(text):
+                let model = OCRResultModel(id: UUID(), createdOn: Date(), ocrResult: text)
+                self?.resultsMockData.append(model)
+                completion(.success(model))
+            }
+        }
     }
     
 }
