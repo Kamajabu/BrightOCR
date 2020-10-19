859734d751c2a97471f629aa192bab2151b392f4
Kamajabu <kamil.buczel@kamajabu.pl>
2020-10-19 15:09:47 +0200

    Added convenience init to coreDataModel

--- BrightOCR/Persistance/OCRResult+CoreDataProperties.swift
+++ BrightOCR/Persistance/OCRResult+CoreDataProperties.swift
@@ -9,7 +9,6 @@
 import Foundation
 import CoreData
 
-
 extension OCRResult {
 
     @nonobjc public class func fetchRequest() -> NSFetchRequest<OCRResult> {
@@ -19,5 +18,12 @@ extension OCRResult {
     @NSManaged public var id: UUID?
     @NSManaged public var createdOn: Date?
     @NSManaged public var ocrText: String?
+    
+    convenience init(in context: NSManagedObjectContext, model: OCRResultModel) {
+        self.init(context: context)
+        self.id = model.id
+        self.createdOn = model.createdOn
+        self.ocrText = model.ocrText
+    }
 
 }
